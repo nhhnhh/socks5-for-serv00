b39dc536c1312cf0ba46a597b3ace43f0fa2bca2
nhhnhh <nhhnhh@msn.com>
2025-03-13 11:46:42 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -29,7 +29,7 @@ else
     echo "添加 socks5 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
-    (crontab -l | grep -F "@reboot bash /home/Ensionersy7414/serv00-play/start.sh > /dev/null 2>&1) || (crontab -l; echo "@reboot bash /home/Ensionersy7414/serv00-play/start.sh > /dev/null 2>&1") | crontab -
+    (crontab -l | grep -F "@reboot bash /home/Ensionersy7414/serv00-play/start.sh > /dev/null 2>&1") || (crontab -l; echo "@reboot bash /home/Ensionersy7414/serv00-play/start.sh > /dev/null 2>&1") | crontab -
     (crontab -l | grep -F "*/30 * * * * bash /home/Ensionersy7414/serv00-play/keepalive.sh > /dev/null 2>&1") || (crontab -l; echo "*/30 * * * * bash /home/Ensionersy7414/serv00-play/keepalive.sh > /dev/null 2>&1") | crontab -
   fi
 fi
