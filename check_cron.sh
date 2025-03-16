b9a99025179e04595bd59b4f276e72508f147d83
nhhnhh <nhhnhh@msn.com>
2025-03-16 11:26:43 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -3,12 +3,12 @@
 USER=$(whoami)
 WORKDIR="/home/${USER}/.nezha-agent"
 FILE_PATH="/home/${USER}/.s5"
+FILE_PATH1="/home/${USER}/serv00-play/singbox"
 CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
 CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
 PM2_PATH="/home/${USER}/.npm-global/lib/node_modules/pm2/bin/pm2"
 CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 CRON_SB="nohup ${FILE_PATH1}/serv00sb run -c ${FILE_PATH1}/config.json >/dev/null 2>&1 &"
-FILE_PATH1="/home/${USER}/serv00-play/singbox"
 REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 
 echo "检查并添加 crontab 任务"
@@ -25,8 +25,8 @@ else
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
     #(crontab -l | grep -F "@reboot bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") || (crontab -l; echo "@reboot bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") | crontab -
     #(crontab -l | grep -F "*/30 * * * * bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") || (crontab -l; echo "*/30 * * * * bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") | crontab -
-    (crontab -l | grep -F "@reboot top  | grep serv00sb > /dev/null  || eval  ${CRON_SB}") ||   (crontab -l; echo "@reboot top  | grep serv00sb > /dev/null ||eval  ${CRON_SB}") | crontab -
-    (crontab -l | grep -F "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}")||  (crontab -l; echo "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}") | crontab -
+    #(crontab -l | grep -F "@reboot top  | grep serv00sb > /dev/null  || eval  ${CRON_SB}") ||   (crontab -l; echo "@reboot top  | grep serv00sb > /dev/null ||eval  ${CRON_SB}") | crontab -
+    #(crontab -l | grep -F "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}")||  (crontab -l; echo "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}") | crontab -
   
   elif [ -e "${WORKDIR}/start.sh" ]; then
     echo "添加 nezha 的 crontab 重启任务"
@@ -36,9 +36,13 @@ else
     echo "添加 socks5 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
-    (crontab -l | grep -F "@reboot top  | grep serv00sb > /dev/null  || eval  ${CRON_SB}") ||   (crontab -l; echo "@reboot top  | grep serv00sb > /dev/null ||eval  ${CRON_SB}") | crontab -
-    (crontab -l | grep -F "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}")||  (crontab -l; echo "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}") | crontab -
+    #(crontab -l | grep -F "@reboot top  | grep serv00sb > /dev/null  || eval  ${CRON_SB}") ||   (crontab -l; echo "@reboot top  | grep serv00sb > /dev/null ||eval  ${CRON_SB}") | crontab -
+    #(crontab -l | grep -F "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}")||  (crontab -l; echo "*/5 * * * * top  | grep serv00sb > /dev/null || eval  ${CRON_SB}") | crontab -
     #(crontab -l | grep -F "@reboot bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") || (crontab -l; echo "@reboot bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") | crontab -
     #(crontab -l | grep -F "*/30 * * * * bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") || (crontab -l; echo "*/30 * * * * bash /home/$(whoami)/serv00-play/singbox/start.sh > /dev/null 2>&1") | crontab -
+  elif [ -e "${FILE_PATH1}/config.json" ]; then
+    echo "添加 sb 的 crontab 重启任务"
+    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && bash ${FILE_PATH1}/sb.sh") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && bash ${FILE_PATH1}/sb.sh") | crontab -
+    (crontab -l | grep -F "*/5 * * * * bash ${FILE_PATH1}/sb.sh") || (crontab -l; echo "*/5 * * * * bash ${FILE_PATH1}/sb.sh") | crontab -
   fi
 fi
