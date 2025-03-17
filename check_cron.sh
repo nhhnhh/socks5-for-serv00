326033765190e30094fdd76ce14913ac318d8940
nhhnhh <nhhnhh@msn.com>
2025-03-17 10:36:30 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -49,7 +49,7 @@ else
 fi
 echo "检查并添加和启动 sb 任务"
 if [ -e "${FILE_PATH1}/config.json" ]; then
-    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && bash ${FILE_PATH1}/sb.sh") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && bash ${FILE_PATH1}/sb.sh") | crontab -
+    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && bash ${FILE_PATH1}/sb.sh > /dev/null") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && bash ${FILE_PATH1}/sb.sh > /dev/null") | crontab -
     (crontab -l | grep -F "*/5 * * * * bash ${FILE_PATH1}/sb.sh") || (crontab -l; echo "*/5 * * * * bash ${FILE_PATH1}/sb.sh") | crontab -
     bash ${FILE_PATH1}/sb.sh
   fi
